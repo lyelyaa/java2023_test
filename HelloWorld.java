8751b64de8853dd9512aff817beedb02ae7e43ee
lyelyaa <koziyolga16@gmail.com>
2023-05-09 00:50:30 +0500

    task 1.2 maybe update tomorrow

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner name = new Scanner(System.in);
+        System.out.println("Hello " + name.nextLine() + "!");
+
+        Map<Double, Double> results = new HashMap<>();
+        double angle = 0.0;
+        double radians;
+
+        for (int i = 0; i <= 9; ++i) {
+            radians = Math.toRadians(angle);
+            results.put(angle, Math.sin(radians));
+            angle += 5;
+        }
+
+        System.out.println("-----------------------------");
+        System.out.printf("%6s %6s", "Angle", "Mean");
+        System.out.println();
+        System.out.println("-----------------------------");
+        for (Map.Entry<Double, Double> result : results.entrySet()){
+            System.out.format(" %-5s | %-15s\n", result.getKey(), result.getValue());
+        }
     }
 }
