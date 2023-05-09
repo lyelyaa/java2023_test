4eaf80f63b5a4f6b66a05f82f12af9ae25072680
lyelyaa <koziyolga16@gmail.com>
2023-05-09 20:31:30 +0500

    update task 1.2 replaced maps with arrays

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,13 +5,15 @@ public class HelloWorld {
         Scanner name = new Scanner(System.in);
         System.out.println("Hello " + name.nextLine() + "!");
 
-        Map<Double, Double> results = new HashMap<>();
+        ArrayList<Double> angles = new ArrayList<>();
+        ArrayList<Double> means = new ArrayList<>();
         double angle = 0.0;
         double radians;
 
         for (int i = 0; i <= 9; ++i) {
+            angles.add(angle);
             radians = Math.toRadians(angle);
-            results.put(angle, Math.sin(radians));
+            means.add(Math.sin(radians));
             angle += 5;
         }
 
@@ -19,8 +21,9 @@ public class HelloWorld {
         System.out.printf("%6s %6s", "Angle", "Mean");
         System.out.println();
         System.out.println("-----------------------------");
-        for (Map.Entry<Double, Double> result : results.entrySet()){
-            System.out.format(" %-5s | %-15s\n", result.getKey(), result.getValue());
+
+        for (int i = 0; i <= 9; ++i) {
+            System.out.format(" %-5s | %-15s\n", angles.get(i), means.get(i));
         }
     }
 }
